10e32014fecfbb1f5660f7eca0d92a0843c5c235
Brian Ambeyi <brian.ambeyi@mitchellcotts.co.ke>
2025-01-18 20:58:21 +0300

    Initial commit for backend

--- HomeFinderBackend/settings.py
+++ HomeFinderBackend/settings.py
@@ -17,14 +17,11 @@ ALLOWED_HOSTS = ['*']
 # CORS_ALLOW_ALL_ORIGINS = True
 
 # CORS Configuration
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://\w+\.your-domain\.com$",
 ]
-
-if DEBUG:
-    CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
     'DELETE',
